b8e844b0b5327c3e2f068321f7e2cab0d53a9aab
yjl <2522160738@qq.com>
2018-11-27 22:04:27 +0800

    11月27日提交

--- public/back/js/isLogin.js
+++ public/back/js/isLogin.js
@@ -1,14 +1,13 @@
-$(function () {
-    $.ajax({
-        url:'/employee/checkRootLogin',
-        type:'get',
-        dataType:'json',
-        success:function (info) {
-            if(info.success){
-                console.log("用户已登录");
-            }else{
-                location.href="login.html";
-            }
+$.ajax({
+    url: '/employee/checkRootLogin',
+    type: 'get',
+    dataType: 'json',
+    success: function (info) {
+        if (info.success) {
+            console.log("用户已登录");
         }
-    })
+        if (info.error === 400) {
+            location.href = "login.html";
+        }
+    }
 })
