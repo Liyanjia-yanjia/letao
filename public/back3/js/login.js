81996a6f752e2133554de1fed54b99bb9111e9ae
yjl <2522160738@qq.com>
2018-11-27 10:39:45 +0800

    back3登陆一半提交

---
+++ public/back3/js/login.js
@@ -0,0 +1,72 @@
+$(function () {
+    $("#form").bootstrapValidator({
+
+        // 配置校验图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok', // 校验成功
+            invalid: 'glyphicon glyphicon-remove', // 校验失败
+            validating: 'glyphicon glyphicon-refresh' // 校验中
+        },
+
+        //表单校验
+        fields: {
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: "用户名不能为空",
+                    },
+                    stringLength: {
+                        min: 2,
+                        max: 6,
+                        message: "长度必须为2-6位"
+                    }
+                }
+            },
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: "密码不能为空",
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 12,
+                        message: "长度必须为6-12位"
+                    }
+                }
+            }
+        },
+        
+    })
+
+    //校验成功
+    $("#form").on('success.form.bv',function (e) {
+        //阻止默认跳转
+        e.preventDefault();
+        //发送请求
+        $.ajax({
+            url:'/employee/employeeLogin',
+            data:$("#form").serialize(),
+            type:'post',
+            dataType:'json',
+            success:function (info) {
+                console.log(info);
+                if(info.error === 1000){
+                    alert("用户名不存在！");
+                }
+                if(info.error === 1001){
+                    alert("密码错误！");
+                }
+                if(info.success){
+                    location.href = 'index.html';
+                }
+            }
+        })
+    })
+
+    //重置按钮处理
+    $('[type="reset"]').on('click',function () {
+        //reset按钮本身会重置内容
+        $("#form").data('bootstrapValidator').resetForm(true);
+        //不传参=>重置内容，传参=>重置状态
+    })
+})
