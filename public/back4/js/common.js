51db0feed89ddd5fd32abea2468fc08901de8506
yjl <2522160738@qq.com>
2018-11-29 10:49:29 +0800

    首页静态页面一半完成

--- public/back4/js/common.js
+++ public/back4/js/common.js
@@ -4,3 +4,33 @@ $(document).ajaxStart(function () {
 $(document).ajaxStop(function () {
     NProgress.done();
 })
+
+$(function () {
+    $(".lt_aside .category").on('click',function () {
+        $(this).next().stop().slideToggle();
+    })
+
+    $(".lt_contain .header .pull-left").on('click',function () {
+        $(".lt_aside").toggleClass("hide_aside");
+        $(".lt_contain").toggleClass("hide_contain");
+        $(".header").toggleClass("hide_header");
+    })
+
+    $(".lt_contain .header .pull-right").on('click',function () {
+        $('#myModal').modal('show')
+    })
+
+    $(".modal .log-out").on('click',function () {
+        $.ajax({
+            url:'/employee/employeeLogout',
+            dataType:'json',
+            type:'get',
+            success:function (info) {
+                console.log(info);
+                if(info.success){
+                    location.href="login.html";
+                }
+            }
+        })
+    }) 
+})
