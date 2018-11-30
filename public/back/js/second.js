02f65bc2541e3b4448f7a06d33e801364e4a6ecf
yjl <2522160738@qq.com>
2018-11-30 09:40:02 +0800

    二级分类基本完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -16,14 +16,14 @@ $(function () {
             type: 'get',
             success: function (info) {
                 // console.log(info);
-                var htmlStr = template("secondTpl",info);
+                var htmlStr = template("secondTpl", info);
                 $("tbody").html(htmlStr);
 
                 $("#paginator").bootstrapPaginator({
-                    bootstrapMajorVersion:3,
-                    currentPage:currentPage,
-                    totalPages:Math.ceil(info.total/info.size),
-                    onPageClicked:function (a,b,c,page) {
+                    bootstrapMajorVersion: 3,
+                    currentPage: currentPage,
+                    totalPages: Math.ceil(info.total / info.size),
+                    onPageClicked: function (a, b, c, page) {
                         currentPage = page;
                         render();
                     }
@@ -32,22 +32,82 @@ $(function () {
         })
     }
 
-    $(".add-btn").on('click',function () {
-         $("#secondModal").modal("show");
-         $.ajax({
-             url:'/category/queryTopCategoryPaging',
-             type:'get',
-             dataType:'json',
-             data:{
-                 page:1,
-                 pageSize:100,
-             },
-             success:function (info) {
-                console.log(info);
-                var htmlStr = template("cateTpl",info);
+    $(".add-btn").on('click', function () {
+        $("#secondModal").modal("show");
+        $.ajax({
+            url: '/category/queryTopCategoryPaging',
+            type: 'get',
+            dataType: 'json',
+            data: {
+                page: 1,
+                pageSize: 100,
+            },
+            success: function (info) {
+                // console.log(info);
+                var htmlStr = template("cateTpl", info);
                 $(".dropdown-menu").html(htmlStr);
-             }
+            }
 
-         })
+        })
+    })
+
+    $(".dropdown-menu").on('click', 'a', function () {
+        var value = $(this).text();
+        // input标签获取值val(),其他标签获取值text()
+        // console.log(value);
+        $(".dropdownTxt").text(value);
+
+        var id = $(this).parent().data("id");
+        $("[name='categoryId']").val(id);
+        console.log(id);
+        $("#secondForm").data('bootstrapValidator').updateStatus('categoryId','VALID')
+       
+    })
+
+    $("#fileupload").fileupload({
+        dataType:'json',
+        done:function (e,data) {
+            // console.log(data);
+            $("#imgBox img").attr("src",data.result.picAddr);
+            $("#secondForm").data('bootstrapValidator').updateStatus('brandLogo','VALID')
+        }
+    })
+
+    $("#secondForm").bootstrapValidator({
+
+        //校验隐藏域
+        excluded:[],
+
+        // 配置校验图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok', // 校验成功
+            invalid: 'glyphicon glyphicon-remove', // 校验失败
+            validating: 'glyphicon glyphicon-refresh' // 校验中
+        },
+
+        //校验
+        fields:{
+            categoryId:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入一级分类名"
+                    }
+                }
+            },
+            categoryName:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入二级分类名"
+                    }
+                }
+            },
+            brandLogo:{
+                validators:{
+                    notEmpty:{
+                        message:"请上传图片"
+                    }
+                }
+            }
+        }
     })
 })
