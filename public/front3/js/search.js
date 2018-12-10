8ebc72cf87036d322c32261e904099abbe38363e
yjl <2522160738@qq.com>
2018-12-10 12:01:14 +0800

    删除一条记录

--- public/front3/js/search.js
+++ public/front3/js/search.js
@@ -9,11 +9,11 @@ $(function () {
     render();
     function render() {
         var arr = getHistory();
-        console.log(arr);
         var htmlStr = template("tmp",{list:arr});
         $(".history").html(htmlStr);
     }
 
+    //清空所有
     $(".history").on("click",".empty",function () {
         mui.confirm("你确定要清空记录吗","温馨提示",["取消","确定"],function (e) {
             if(e.index === 1){
@@ -21,5 +21,15 @@ $(function () {
                 render();
             }
         })
+    })
+
+    //删除一条记录
+    $(".history").on("click",".btn_delete",function () {
+        var arr = getHistory();
+        arr.shift();
+        var str = JSON.stringify(arr);
+        localStorage.setItem("search_list",str);
+        var arr = getHistory();
+        render();
     })
 })
