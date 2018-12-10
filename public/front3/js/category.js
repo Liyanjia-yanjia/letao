2024d882b15c8785253e2c6fe86d19c75ba58450
yjl <2522160738@qq.com>
2018-12-10 10:38:27 +0800

    分类页完成

--- public/front3/js/category.js
+++ public/front3/js/category.js
@@ -6,28 +6,35 @@ $(function () {
         dataType:'json',
         success:function (info) {
             console.log(info);
-
             var htmlStr = template("cateTpl",info);
             $(".nav_left ul").html(htmlStr);
         }
     })
 
     //点击a使其高亮
-    $(".nav_left ul").on('click','a',function () {
-        $(".nav_left ul a").removeClass("current");
-        $(this).addClass("current");
-        //获取二级分类
-        var id = $(this).data("id");
+    var id;
+    render();
+    function render() {
         $.ajax({
             url:'/category/querySecondCategory',
             data:{
-                id:id,
+                id: id || 1,
             },
             dataType:'json',
             type:'get',
             success:function (info) {
                 console.log(info);
+                var htmlStr = template("categoryTpl",info);
+                $(".nav_right ul").html(htmlStr);
             }
         })
+    }
+    $(".nav_left ul").on('click','a',function () {
+        $(".nav_left ul a").removeClass("current");
+        $(this).addClass("current");
+        //获取二级分类
+        id = $(this).data("id");
+        render();
+        
     })
 })
